e33c4b1ec8a02fba9824e2b154bb49540334db1e
Thang Luu <df.thangld@hotmail.com>
2020-01-03 23:02:08 +0700

    Skeleton for web application

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+DEBUG = False
+
+ADMINS = frozenset(['df.thangld@hotmail.com'])
+SECRET_KEY = '319cf9cbfcea4c3892b680bc79560113'
+
+SQLALCHEMY_DATABASE_URI = 'Database connection string'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
